04a33ec94649dde55680c7d3e0534f5c41c9e76c
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-05-16 17:42:21 +0200

    Add LoginFragment, which take you to login page

--- app/src/main/java/com/example/android_mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/android_mobile_app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.android_mobile_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_mobile_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
