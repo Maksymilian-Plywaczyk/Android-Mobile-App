bb2b9e0dda340a6add7aa5dba75728336ef6ee43
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-05-17 13:11:03 +0200

    Add 2 new fragments register and login

--- app/src/main/java/com/example/android_mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/android_mobile_app/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.android_mobile_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.android_mobile_app.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
     }
 }
