34bca72ff27f7f8352720321640de64c641b4e26
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-05-17 16:03:50 +0200

    Develop login and register page with Firebase

--- app/src/main/java/com/example/android_mobile_app/LoginFragment.kt
+++ app/src/main/java/com/example/android_mobile_app/LoginFragment.kt
@@ -1,21 +1,79 @@
 package com.example.android_mobile_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.android_mobile_app.databinding.FragmentLoginBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.Dispatchers.Main
 
 
 class LoginFragment : Fragment() {
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var binding: FragmentLoginBinding
+    private var email = ""
+    private var password = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
-            R.layout.fragment_login,container,false)
+        binding = DataBindingUtil.inflate<FragmentLoginBinding>(
+            inflater,
+            R.layout.fragment_login, container, false
+        )
+        mAuth = Firebase.auth
+        checkUser()
+        binding.newUserText.setOnClickListener {
+            startActivity(Intent(activity, RegisterFragment::class.java))
+        }
+        binding.loginButton.setOnClickListener {
+            validateData()
+        }
         return binding.root
+    }
+
+    private fun validateData() {
+        email = binding.loginEmail.text.toString().trim()
+        password = binding.loginPassword.text.toString().trim()
+
+        //validate Data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.loginEmail.error = "Invalid email format"
+        } else if (TextUtils.isEmpty(password)) {
+            binding.loginPassword.error = "Please enter password"
+        } else {
+            firebaseLogin()
+        }
+    }
+
+    private fun firebaseLogin() {
+        mAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+            val firebaseUser = mAuth.currentUser
+            val email = firebaseUser!!.email
+            Toast.makeText(activity, "LoggedIn as $email", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(activity, MainActivity::class.java))
+            activity?.finish()
+        }.addOnFailureListener { e ->
+            Toast.makeText(activity, "Login failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = mAuth.currentUser
+        if (firebaseUser != null) {
+            startActivity(Intent(activity, MainActivity::class.java))
+            activity?.finish()
+        }
     }
 }
--- app/src/main/java/com/example/android_mobile_app/RegisterFragment.kt
+++ app/src/main/java/com/example/android_mobile_app/RegisterFragment.kt
@@ -1,59 +1,71 @@
 package com.example.android_mobile_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.android_mobile_app.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentRegisterBinding
+    private lateinit var mAuth: FirebaseAuth
+    private var email = ""
+    private var password = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = DataBindingUtil.inflate<FragmentRegisterBinding>(
+            inflater,
+            R.layout.fragment_login, container, false
+        )
+        mAuth = Firebase.auth
+        binding.signUpButton.setOnClickListener {
+            validateData()
+        }
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun validateData() {
+        email = binding.loginEmail.toString().trim()
+        password = binding.loginPassword.toString().trim()
+
+        //validate Data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.loginEmail.error = "Invalid email format"
+        } else if (TextUtils.isEmpty(password)) {
+            binding.loginPassword.error = "Please enter password"
+        } else if (password.length < 6) {
+            binding.loginPassword.error = "Password must atleast 6 characters long"
+        } else {
+            firebaseSingUp()
+        }
     }
+
+    private fun firebaseSingUp() {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+            val firebaseUser = mAuth.currentUser
+            val email = firebaseUser!!.email
+            Toast.makeText(activity, "SingUp as $email", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(activity, MainActivity::class.java))
+            activity?.finish()
+        }.addOnFailureListener { e ->
+            Toast.makeText(activity, "SingUp  failed due to ${e.message}", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
 }
