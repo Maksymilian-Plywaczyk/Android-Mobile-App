08b51b21bd38b5e667fe8222f798033d76d9594f
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-05-18 11:50:20 +0200

    Add navigation drawer

--- app/src/main/java/com/example/android_mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/android_mobile_app/MainActivity.kt
@@ -2,17 +2,30 @@ package com.example.android_mobile_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android_mobile_app.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth;
+    private lateinit var auth: FirebaseAuth
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding  = DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView,navController)
         auth = Firebase.auth
 
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 }
