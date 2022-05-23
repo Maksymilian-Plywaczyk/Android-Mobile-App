f1a1241ba0a94364b1e35faa53460d6c81b70b63
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-05-23 22:40:04 +0200

    Developing sensors fragment

--- app/src/main/java/com/example/android_mobile_app/SensorsFragment.kt
+++ app/src/main/java/com/example/android_mobile_app/SensorsFragment.kt
@@ -1,20 +1,55 @@
 package com.example.android_mobile_app
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.android_mobile_app.databinding.FragmentSensorsBinding
 
 
-class SensorsFragment : Fragment() {
+     class SensorsFragment : Fragment(),SensorEventListener  {
    private lateinit var binding:FragmentSensorsBinding
+   private lateinit var sensorManager:SensorManager
+
+   private var accelerometer: Sensor? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+
         return inflater.inflate(R.layout.fragment_sensors, container, false)
+
+    }
+    override fun onResume() {
+        super.onResume()
+        accelerometer?.also { sensor ->
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
     }
 }
